smartcoop/design-library

Add implementation of the Smart design button toolbar
https://design.smart.coop/development/docs/c-button-toolbar.html.

src/Smart.Design.Razor/TagHelpers/Toolbar/ButtonToolbarHtmlGenerator.cs
@@ -1,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.Toolbar;
+
+public class ButtonToolbarHtmlGenerator : IButtonToolbarHtmlGenerator
+{
+
+    /// <inheritdoc />
+    public TagBuilder GenerateSmartButtonToolbar(ButtonToolbarLayout layout, bool isCompact)
+    {
+        var toolbar = new TagBuilder("div");
+        toolbar.AddCssClass("c-button-toolbar");
+
+        if (layout == ButtonToolbarLayout.Vertical)
+        {
+            toolbar.AddCssClass("c-button-toolbar--vertical");
+        }
+
+        if (isCompact)
+        {
+            toolbar.AddCssClass("c-button-toolbar--compact");
+        }
+
+        return toolbar;
+    }
+}

src/Smart.Design.Razor/TagHelpers/Toolbar/ButtonToolbarLayout.cs
@@ -1,0 +1,7 @@
+namespace Smart.Design.Razor.TagHelpers.Toolbar;
+
+public enum ButtonToolbarLayout
+{
+    Horizontal,
+    Vertical
+}

src/Smart.Design.Razor/TagHelpers/Toolbar/IButtonToolbarHtmlGenerator.cs
@@ -1,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.Toolbar;
+
+public interface IButtonToolbarHtmlGenerator
+{
+
+    /// <summary>
+    /// Generate a div that represents the smart button toolbar.
+    /// Documentation is available <see href="https://design.smart.coop/development/docs/c-button-toolbar.html">here</see>.
+    /// </summary>
+    /// <param name="layout">The orientation of the item inside the toolbar. Value are.</param>
+    /// <param name="isCompact">Tells is there should be no space between items.</param>
+    /// <returns>A instance of a smart button toolbar</returns>
+    TagBuilder GenerateSmartButtonToolbar(ButtonToolbarLayout layout, bool isCompact);
+}

Khaon/design-cs

src/Smart.Design.Razor/TagHelpers/ButtonToolbar/ButtonToolbarHtmlGenerator.cs
@@ -1,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.ButtonToolbar;
+
+public class ButtonToolbarHtmlGenerator : IButtonToolbarHtmlGenerator
+{
+
+    /// <inheritdoc />
+    public TagBuilder GenerateSmartButtonToolbar(ButtonToolbarLayout layout, bool isCompact)
+    {
+        var toolbar = new TagBuilder("div");
+        toolbar.AddCssClass("c-button-toolbar");
+
+        if (layout == ButtonToolbarLayout.Vertical)
+        {
+            toolbar.AddCssClass("c-button-toolbar--vertical");
+        }
+
+        if (isCompact)
+        {
+            toolbar.AddCssClass("c-button-toolbar--compact");
+        }
+
+        return toolbar;
+    }
+}

src/Smart.Design.Razor/TagHelpers/ButtonToolbar/ButtonToolbarLayout.cs
@@ -1,0 +1,7 @@
+namespace Smart.Design.Razor.TagHelpers.ButtonToolbar;
+
+public enum ButtonToolbarLayout
+{
+    Horizontal,
+    Vertical
+}

src/Smart.Design.Razor/TagHelpers/ButtonToolbar/IButtonToolbarHtmlGenerator.cs
@@ -1,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Smart.Design.Razor.TagHelpers.ButtonToolbar;
+
+public interface IButtonToolbarHtmlGenerator
+{
+
+    /// <summary>
+    /// Generate a div that represents the smart button toolbar.
+    /// Documentation is available <see href="https://design.smart.coop/development/docs/c-button-toolbar.html">here</see>.
+    /// </summary>
+    /// <param name="layout">The orientation of the item inside the toolbar. Value are.</param>
+    /// <param name="isCompact">Tells is there should be no space between items.</param>
+    /// <returns>A instance of a smart button toolbar</returns>
+    TagBuilder GenerateSmartButtonToolbar(ButtonToolbarLayout layout, bool isCompact);
+}