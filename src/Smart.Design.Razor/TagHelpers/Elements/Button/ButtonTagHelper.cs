82cd97a4744cb7c5b5f3bf22189b42d410ae59e5
Maxime Poulain <maxime.poulain@smart.coop>
2022-02-01 16:43:19 +0100

    fixup! Add implementation of the Smart design button

--- src/Smart.Design.Razor/TagHelpers/Elements/Button/ButtonTagHelper.cs
+++ src/Smart.Design.Razor/TagHelpers/Elements/Button/ButtonTagHelper.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.AspNetCore.Routing;
 using Smart.Design.Razor.TagHelpers.Base;
 using Smart.Design.Razor.TagHelpers.Constants;
 using Smart.Design.Razor.TagHelpers.Icon;
@@ -31,7 +35,20 @@ namespace Smart.Design.Razor.TagHelpers.Elements.Button;
 [HtmlTargetElement(TagNames.ButtonTagName, TagStructure = TagStructure.NormalOrSelfClosing)]
 public class ButtonTagHelper : BaseTagHelper
 {
+    private const string FormAction = "formaction";
+
+    private const string FragmentAttributeName = "asp-fragment";
+    private const string PageAttributeName = "asp-page";
+    private const string PageHandlerAttributeName = "asp-page-handler";
+    private const string RouteAttributeName = "asp-route";
+    private const string RouteValuesDictionaryName = "asp-all-route-data";
+    private const string RouteValuesPrefix = "asp-route-";
+
+    private IDictionary<string, string>? _routeValues;
+
     private readonly IIconHtmlGenerator _iconHtmlGenerator;
+    private readonly IUrlHelperFactory _urlHelperFactory;
+
 
     [HtmlAttributeName("disabled")]
     public bool Disabled { get; set; }
@@ -57,9 +74,47 @@ public class ButtonTagHelper : BaseTagHelper
     [HtmlAttributeName("icon-only")]
     public bool IconOnly { get; set; }
 
-    public ButtonTagHelper(IIconHtmlGenerator iconHtmlGenerator)
+    /// <summary>
+    /// Gets or sets the URL fragment.
+    /// </summary>
+    [HtmlAttributeName(FragmentAttributeName)]
+    public string? Fragment { get; set; }
+
+    /// <summary>
+    /// Attribute helper to specify routes such as asp-route-id.
+    /// </summary>
+    /// <remarks>
+    /// This attributes doesn't do anything as it is used only to help developer with autocomplete.
+    /// </remarks>
+    [HtmlAttributeName(RouteAttributeName)]
+    public string? Route { get; set; }
+
+    /// <summary>
+    /// Additional parameters for the route.
+    /// </summary>
+    [HtmlAttributeName(RouteValuesDictionaryName, DictionaryAttributePrefix = RouteValuesPrefix)]
+    public IDictionary<string, string> RouteValues
+    {
+        get => _routeValues ??= new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        set => _routeValues = value;
+    }
+
+    /// <summary>
+    /// The name of the page.
+    /// </summary>
+    [HtmlAttributeName(PageAttributeName)]
+    public string? Page { get; set; }
+
+    /// <summary>
+    /// The name of the page handler.
+    /// </summary>
+    [HtmlAttributeName(PageHandlerAttributeName)]
+    public string? PageHandler { get; set; }
+
+    public ButtonTagHelper(IIconHtmlGenerator iconHtmlGenerator, IUrlHelperFactory urlHelperFactory)
     {
         _iconHtmlGenerator = iconHtmlGenerator;
+        _urlHelperFactory = urlHelperFactory;
     }
 
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
@@ -88,6 +143,7 @@ public class ButtonTagHelper : BaseTagHelper
             output.AddClass("c-button--block", HtmlEncoder.Default);
         }
 
+        // The content of a smart button is actually a span.
         var buttonContent = new TagBuilder("span");
         buttonContent.AddCssClass("c-button__content");
 
@@ -98,7 +154,7 @@ public class ButtonTagHelper : BaseTagHelper
             buttonContent.InnerHtml.AppendHtml(leadingIcon);
         }
 
-        // If the attribute icon-only is not set to true, we expect to put a span.
+        // If the attribute icon-only is not set to true, we expect to put the label inside a span.
         if (!IconOnly)
         {
             var spanButtonLabelTagBuilder = new TagBuilder("span");
@@ -127,6 +183,9 @@ public class ButtonTagHelper : BaseTagHelper
                 iconOnlyDiv.InnerHtml.SetContent(Label);
         }
 
+        // If there is any custom handler specified we need to add its route to to the attribute formaction.
+        SetFormAction(output);
+
         output.Content.SetHtmlContent(buttonContent);
     }
 
@@ -134,12 +193,30 @@ public class ButtonTagHelper : BaseTagHelper
     {
         return Style switch
         {
-            ButtonStyle.Primary => "c-button--primary",
-            ButtonStyle.Secondary => "c-button--secondary",
-            ButtonStyle.Borderless => "c-button--borderless",
-            ButtonStyle.Danger => "c-button--danger",
+            ButtonStyle.Primary         => "c-button--primary",
+            ButtonStyle.Secondary       => "c-button--secondary",
+            ButtonStyle.Borderless      => "c-button--borderless",
+            ButtonStyle.Danger          => "c-button--danger",
             ButtonStyle.DangerSecondary => "c-button--danger-secondary",
-            _ => throw new NotImplementedException($"Style undefined for style {Style}")
+            _                           => throw new NotImplementedException($"Style undefined for style {Style}")
         };
+    }
+
+    private void SetFormAction(TagHelperOutput output)
+    {
+        // Checks if a custom handler has been set.
+        var hasCustomPageLink = Page != null || PageHandler != null;
+        if (!hasCustomPageLink)
+            return;
+
+        RouteValueDictionary? routeValues = null;
+        if (RouteValues is {Count: > 0})
+        {
+            routeValues = new RouteValueDictionary(RouteValues);
+        }
+
+        var urlHelper = _urlHelperFactory.GetUrlHelper(ViewContext!);
+        var url = urlHelper.Page(Page, PageHandler, routeValues, null, null, Fragment);
+        output.Attributes.SetAttribute(FormAction, url);
     }
 }
